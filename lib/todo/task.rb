26e1975311006d14e37761a505bcb3058a9e7fec
dddisk <13da144@gmail.com>
2017-07-31 14:34:54 +0900

    add methods

--- lib/todo/task.rb
+++ lib/todo/task.rb
@@ -3,6 +3,8 @@ require 'active_record'
 module Todo
   class Task < ActiveRecord::Base
 
+    scope :status_is, ->(status) {where(status: status)}
+
     NOT_YET = 0 #タスクが完了していない
     DONE = 1 #タスクが完了した
     PENDING = 2 #保留状態のタスク
