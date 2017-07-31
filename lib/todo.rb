fe9449aff58c399f2f0d40b4daac15e62ca32114
dddisk <13da144@gmail.com>
2017-07-31 15:02:42 +0900

    add command

--- lib/todo.rb
+++ lib/todo.rb
@@ -2,6 +2,7 @@ require "todo/version"
 require "todo/command"
 require "todo/db"
 require "todo/task"
+require "todo/command/options"
 
 module Todo
   # Your code goes here...
--- lib/todo/command.rb
+++ lib/todo/command.rb
@@ -2,7 +2,16 @@ module Todo
 
   class Command
 
+    def self.run(argv)
+      new(argv).execute
+    end
+
+    def initialize(argv)
+      @argv = argv
+    end
+
     def execute
+      options = Options.parse!(@argv)
       DB.prepare
     end
 
